77ec961e88efb6f68e30468856bfd072b4d7c558
scancetin <can.cetinbas@ogr.sakarya.edu.tr>
2021-08-23 14:38:12 +0300

    page-view

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_examples/file_manager/file_download_screen.dart';
 import 'package:flutter_examples/firebase_api_write/screens/firebase_screen.dart';
 import 'package:flutter_examples/shared_preferences/shared_example.dart';
-import 'package:flutter_examples/widget_examples/screens/collapse_view.dart';
-import 'package:flutter_examples/widget_examples/screens/page_view_screen.dart';
+import 'package:flutter_examples/widget_examples/collapse_view.dart';
+import 'package:flutter_examples/widget_examples/page_view_screen.dart';
 import 'using_http_futurebuilder/screens/using_futurebuilder.dart';
 
 void main() => runApp(MyApp());
